47ae72477f0435f9c7ac994db81a57b9540d16dd
David <singerous@gmail.com>
2019-03-28 15:11:08 +0700

    updated images_preparator.py
    added ability to enter directory and image size

--- image_preparation/images_preparator.py
+++ image_preparation/images_preparator.py
@@ -1,33 +1,32 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import os
 import cv2
 from tqdm import tqdm
 import random
 import pickle
 
-IMAGE_SIZE = 20
-
-DIR = "C:/Users/singe/Desktop/training/generated_base"
-
-CATEGORIES = ["cats", "dogs"]
-
-def create_training_data(training_data):
-    for category in CATEGORIES:
-        path = os.path.join(DIR, category)
-        class_num = CATEGORIES.index(category)
+def create_training_data(training_data, dir, categories, image_size):
+    for category in categories:
+        path = os.path.join(dir, category)
+        class_num = categories.index(category)
 
         for image in tqdm(os.listdir(path)):
             try:
                 image_array = cv2.imread(os.path.join(path, image), cv2.IMREAD_GRAYSCALE)
-                normalized_image_array = cv2.resize(image_array, (IMAGE_SIZE, IMAGE_SIZE))
+                normalized_image_array = cv2.resize(image_array, (image_size, image_size))
                 training_data.append([normalized_image_array, class_num])
 
             except Exception:
                 pass
 
+categories = ["cats", "dogs"]
+print("Enter directory name with data (it must include 2 folders with names \"cats\" and \"dogs\")\n"
+      "e. g. C:/Users/SingeRous/Desktop/training/generated_base")
+dir = input()
+print("Enter image size for reshaping")
+image_size = int(input())
 training_data = []
-create_training_data(training_data)
+create_training_data(training_data, dir, categories, image_size)
 random.shuffle(training_data)
 
 X = []
@@ -37,7 +36,7 @@ for features, label in training_data:
     X.append(features)
     y.append(label)
 
-X = np.array(X).reshape(-1, IMAGE_SIZE, IMAGE_SIZE, 1)
+X = np.array(X).reshape(-1, image_size, image_size, 1)
 pickle_out = open("X.pickle", "wb")
 pickle.dump(X, pickle_out)
 pickle_out.close()
